6480705271a3e968870a361a63e056b5737fcdd4
Koles <foockpw@gmail.com>
2024-03-18 18:57:48 +0100

    newcodepvppvmlistyzbierz1

--- rcodrpvp.js
+++ rcodrpvp.js
@@ -1,4 +1,4 @@
-    let lastTimestamp = Date.now();
+ let lastTimestamp = Date.now();
 let isRunning = false;
 let refreshInterval;
 
@@ -14,23 +14,77 @@ function checkRefresh() {
             isRunning = false;
 
             setTimeout(() => {
-                let ghButtonElement, codeButtonElement;
-
                 if (document.getElementById('checkbox1').checked) {
-                    ghButtonElement = document.querySelector('.gh_button.gh_code');
-                    codeButtonElement = document.querySelector('.code_button.code_code');
+                    let ghButtonElement = document.querySelector('.gh_button.gh_code');
+                    let codeButtonElement = document.querySelector('.code_button.code_code');
+                    
+                    if (ghButtonElement && codeButtonElement) {
+                        setTimeout(() => {
+                            ghButtonElement.click();
+                            setTimeout(() => {
+                                codeButtonElement.click();
+                            }, 2000);
+                        }, 2000);
+                    }
                 } else if (document.getElementById('checkbox2').checked) {
-                    ghButtonElement = document.querySelector('.gh_button.gh_pvp');
-                    codeButtonElement = document.querySelector('.pvp_button.pvp_pvp');
+                    let ghButtonElement = document.querySelector('.gh_button.gh_pvp');
+                    let codeButtonElement = document.querySelector('.pvp_button.pvp_pvp');
+                    
+                    if (ghButtonElement && codeButtonElement) {
+                        setTimeout(() => {
+                            ghButtonElement.click();
+                            setTimeout(() => {
+                                codeButtonElement.click();
+                            }, 2000);
+                        }, 2000);
+                    }
+                } else if (document.getElementById('checkbox3').checked) {
+                    const ghRespButton = document.querySelector('.gh_button.gh_resp');
+                    if (ghRespButton) {
+                        setTimeout(() => {
+                            ghRespButton.click();
+                            setTimeout(() => {
+                                const respButton = document.querySelector('.resp_button.resp_resp');
+                                if (respButton) {
+                                    respButton.click();
+                                }
+                            }, 2000);
+                        }, 2000);
+                    }
+                } else if (document.getElementById('checkbox4').checked) {
+                    const ghLpvmButton = document.querySelector('.gh_button.gh_lpvm');
+                    if (ghLpvmButton) {
+                        setTimeout(() => {
+                            ghLpvmButton.click();
+                            setTimeout(() => {
+                                const lpvmUButton = document.querySelector('.lpvm_button.lpvm_u');
+                                if (lpvmUButton) {
+                                    lpvmUButton.click();
+                                    setTimeout(() => {
+                                        const lpvmLpvmButton = document.querySelector('.lpvm_button.lpvm_lpvm');
+                                        if (lpvmLpvmButton) {
+                                            lpvmLpvmButton.click();
+                                        }
+                                    }, 2000);
+                                }
+                            }, 2000);
+                        }, 2000);
+                    }
+                } else if (document.getElementById('checkbox5').checked) {
+                    const ghResButton = document.querySelector('.gh_button.gh_res');
+                    if (ghResButton) {
+                        setTimeout(() => {
+                            ghResButton.click();
+                            setTimeout(() => {
+                                const resButton = document.querySelector('.res_button.res_res');
+                                if (resButton) {
+                                    resButton.click();
+                                }
+                            }, 2000);
+                        }, 2000);
+                    }
                 }
-
-                if (ghButtonElement && codeButtonElement) {
-                    ghButtonElement.click();
-                    setTimeout(() => {
-                        codeButtonElement.click();
-                    }, 2500);
-                }
-            }, 2500);
+            }, 2000);
         }
     }
 }
@@ -67,42 +121,41 @@ function createControlButton() {
     controlButton.style.right = '10px';
     controlButton.style.background = 'rgba(0,0,0,0.9)';
     controlButton.style.zIndex = '9999';
-    controlButton.style.width = '150px'; // Zmiana szerokości na 50px
+    controlButton.style.width = '150px'; 
     controlButton.style.padding = '1px';
     controlButton.style.borderRadius = '5px';
     controlButton.style.borderStyle = 'solid';
     controlButton.style.borderWidth = '7px 8px 7px 7px';
     controlButton.style.display = 'block';
     controlButton.style.userSelect = 'none';
-    controlButton.style.color = 'gold'; // Zmiana koloru czcionki na gold
-    controlButton.style.borderColor = '#333333'; // Zmiana koloru obramowania na #333333
+    controlButton.style.color = 'gold'; 
+    controlButton.style.borderColor = '#333333'; 
     controlButton.addEventListener('click', () => {
         toggleScript();
     });
     document.body.appendChild(controlButton);
 
-    // Create checkboxes
     const checkboxContainer = document.createElement('div');
     checkboxContainer.style.position = 'absolute';
     checkboxContainer.style.top = '73px';
     checkboxContainer.style.right = '28px';
     checkboxContainer.style.background = 'rgba(0,0,0,0.9)';
     checkboxContainer.style.zIndex = '9999';
-    checkboxContainer.style.width = '100px';
+    checkboxContainer.style.width = '100px'; 
     checkboxContainer.style.padding = '1px';
     checkboxContainer.style.borderRadius = '5px';
     checkboxContainer.style.borderStyle = 'solid';
     checkboxContainer.style.borderWidth = '7px 8px 7px 7px';
     checkboxContainer.style.display = 'block';
     checkboxContainer.style.userSelect = 'none';
-    checkboxContainer.style.color = '#333333'; //
+    checkboxContainer.style.color = '#333333'; 
 
     const checkbox1 = document.createElement('input');
     checkbox1.type = 'checkbox';
     checkbox1.id = 'checkbox1';
-    checkbox1.checked = window.localStorage.getItem('checkbox1_checked') === 'true'; // Ustawienie stanu z localStorage
+    checkbox1.checked = window.localStorage.getItem('checkbox1_checked') === 'true'; 
     checkbox1.addEventListener('change', () => {
-        window.localStorage.setItem('checkbox1_checked', checkbox1.checked); // Zapisanie stanu do localStorage
+        window.localStorage.setItem('checkbox1_checked', checkbox1.checked); 
         if (checkbox1.checked) {
             document.getElementById('checkbox2').checked = false;
         }
@@ -111,14 +164,14 @@ function createControlButton() {
     const label1 = document.createElement('label');
     label1.htmlFor = 'checkbox1';
     label1.textContent = 'R - Kody';
-    label1.style.color = 'gold'; // Zmiana koloru tekstu na gold
-    label1.style.paddingLeft = '10px';    
+    label1.style.color = 'gold'; 
+    label1.style.paddingLeft = '10px';
     const checkbox2 = document.createElement('input');
     checkbox2.type = 'checkbox';
     checkbox2.id = 'checkbox2';
-    checkbox2.checked = window.localStorage.getItem('checkbox2_checked') === 'true'; // Ustawienie stanu z localStorage
+    checkbox2.checked = window.localStorage.getItem('checkbox2_checked') === 'true'; 
     checkbox2.addEventListener('change', () => {
-        window.localStorage.setItem('checkbox2_checked', checkbox2.checked); // Zapisanie stanu do localStorage
+        window.localStorage.setItem('checkbox2_checked', checkbox2.checked); 
         if (checkbox2.checked) {
             document.getElementById('checkbox1').checked = false;
         }
@@ -127,13 +180,64 @@ function createControlButton() {
     const label2 = document.createElement('label');
     label2.htmlFor = 'checkbox2';
     label2.textContent = ' R - PVP';
-    label2.style.color = 'gold'; // Zmiana koloru tekstu na gold
-    label2.style.paddingLeft = '10px';    
+    label2.style.color = 'gold';
+    label2.style.paddingLeft = '10px';
     checkboxContainer.appendChild(checkbox1);
     checkboxContainer.appendChild(label1);
     checkboxContainer.appendChild(document.createElement('br'));
     checkboxContainer.appendChild(checkbox2);
     checkboxContainer.appendChild(label2);
+
+    const checkbox3 = document.createElement('input');
+    checkbox3.type = 'checkbox';
+    checkbox3.id = 'checkbox3';
+    checkbox3.checked = window.localStorage.getItem('checkbox3_checked') === 'true';
+    checkbox3.addEventListener('change', () => {
+        window.localStorage.setItem('checkbox3_checked', checkbox3.checked);
+    });
+
+    const label3 = document.createElement('label');
+    label3.htmlFor = 'checkbox3';
+    label3.textContent = ' R - PVM';
+    label3.style.color = 'gold';
+    label3.style.paddingLeft = '10px';
+    checkboxContainer.appendChild(checkbox3);
+    checkboxContainer.appendChild(label3);
+    checkboxContainer.appendChild(document.createElement('br'));
+
+    const checkbox4 = document.createElement('input');
+    checkbox4.type = 'checkbox';
+    checkbox4.id = 'checkbox4';
+    checkbox4.checked = window.localStorage.getItem('checkbox4_checked') === 'true';
+    checkbox4.addEventListener('change', () => {
+        window.localStorage.setItem('checkbox4_checked', checkbox4.checked);
+    });
+
+    const label4 = document.createElement('label');
+    label4.htmlFor = 'checkbox4';
+    label4.textContent = ' R - Listy';
+    label4.style.color = 'gold';
+    label4.style.paddingLeft = '10px';
+    checkboxContainer.appendChild(checkbox4);
+    checkboxContainer.appendChild(label4);
+    checkboxContainer.appendChild(document.createElement('br'));
+
+    const checkbox5 = document.createElement('input');
+    checkbox5.type = 'checkbox';
+    checkbox5.id = 'checkbox5';
+    checkbox5.checked = window.localStorage.getItem('checkbox5_checked') === 'true';
+    checkbox5.addEventListener('change', () => {
+        window.localStorage.setItem('checkbox5_checked', checkbox5.checked);
+    });
+
+    const label5 = document.createElement('label');
+    label5.htmlFor = 'checkbox5';
+    label5.textContent = 'R - Zbie';
+    label5.style.color = 'gold';
+    label5.style.paddingLeft = '10px';
+    checkboxContainer.appendChild(checkbox5);
+    checkboxContainer.appendChild(label5);
+    checkboxContainer.appendChild(document.createElement('br'));
 
     document.body.appendChild(checkboxContainer);
 }
