a93e6e41c0a305865e3619df4eea07f6fb42760e
Koles1910 <foockpw@gmail.com>
2024-04-05 22:55:23 +0200

    1

--- tlo.js
+++ tlo.js
@@ -3,24 +3,18 @@ let klawiszMinusNacisniety = false;
 document.addEventListener('keydown', function(event) {
     if (!$("input, textarea").is(":focus")) {
         if (event.key === "-" && !klawiszMinusNacisniety) {
-            document.body.style.backgroundSize = 'auto';
-            document.body.style.backgroundRepeat = 'no-repeat';
-            document.body.style.backgroundAttachment = 'fixed';
-            document.body.style.height = (window.innerHeight + 250) + 'px';
-            document.body.style.backgroundPositionX = '-223px';
-            document.body.style.backgroundSize = '120%';
-            document.body.style.zoom = "75%";
+            addToCSS.innerHTML += `
+                body {
+                    background-size: auto;
+                    background-repeat: no-repeat;
+                    background-attachment: fixed;
+                    height: ${(window.innerHeight + 250)}px;
+                    background-position-x: -223px;
+                    background-size: 120%;
+                    zoom: 75%;
+                }
+            `;
             klawiszMinusNacisniety = true;
-        } else if (event.key === "-") {
-
-            document.body.style.backgroundSize = '';
-            document.body.style.backgroundRepeat = '';
-            document.body.style.backgroundAttachment = '';
-            document.body.style.height = '';
-            document.body.style.backgroundPositionX = '';
-            document.body.style.backgroundSize = '';
-            document.body.style.zoom = "";
-            klawiszMinusNacisniety = false;
         }
     }
 });
