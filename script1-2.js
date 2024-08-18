1c875803c2f98fbc495b180300a6bfc4f6debc7d
KWS4ALL <kwsforall@proton.me>
2024-08-18 21:53:03 +0200

    Premka

--- script1-2.js
+++ script1-2.js
@@ -1,6 +1,6 @@
 var checked = false;
 
-if (typeof GAME === 'undefined') { } else {
+if (typeof GAME === 'undefined' && extrapremium) { } else {
     let Pog = setInterval(() => {
         if (!GAME.pid) { } else {
             clearInterval(Pog);
